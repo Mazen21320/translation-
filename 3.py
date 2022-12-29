0f73aca6895772a41d22fb798add71041ca25816
Mazen21320 <zezoa400@gmail.com>
2022-12-29 15:47:29 +0200

    first commit

---
+++ 3.py
@@ -0,0 +1,18 @@
+
+Text = input("enter your text : ")
+
+def translate(str):
+    translation=""
+    for letter in str :
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "Z"
+            else : 
+                translation = translation + "z"     
+        else : 
+            translation = translation + letter 
+    return translation 
+
+print(translate(Text))                
+
+
